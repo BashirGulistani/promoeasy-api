b8af4e35bffc487d4e96f7882e80744f2da3fa58
Bashir Gulistani <76609477+BashirGulistani@users.noreply.github.com>
2026-01-04 22:47:47 -0500

    Update taskCreate.ts

--- src/endpoints/tasks/taskCreate.ts
+++ src/endpoints/tasks/taskCreate.ts
@@ -3,3 +3,12 @@ import { z } from "zod";
 import { HandleArgs } from "../../types";
 import { TaskModel } from "./base";
 
+const TaskCreateSchema = TaskModel.schema
+  .omit({ id: true })
+  .extend({
+    description: z.string().default(""),
+    completed: z.coerce.boolean().default(false),
+    due_date: z.string().datetime().optional().nullable(),
+  });
+
+
