281149a05e119ce527aaf8c4d49c1a8a5c0bb125
Bashir Gulistani <76609477+BashirGulistani@users.noreply.github.com>
2026-01-04 22:43:23 -0500

    Update dummyEndpoint.ts

--- src/endpoints/dummyEndpoint.ts
+++ src/endpoints/dummyEndpoint.ts
@@ -3,3 +3,11 @@ import { AppContext } from "../types";
 import { z } from "zod";
 
 
+
+async function fetchLogDetailsFromSource(args: {
+  slug: string;
+  name: string;
+  c: AppContext;
+}) {
+  const { slug, name, c } = args;
+
